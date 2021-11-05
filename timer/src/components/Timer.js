bb20ec015418650a4008f014bbbbf26f78544b28
Dmnk28 <dominik.oesterle@posteo.de>
2021-11-05 23:56:31 +0100

    Timecalculation and UI-Setup
    5 Hours of Time. First struggeling with the setIntervall then used accurateIntervall, inspired by the FCC-Example. But I didn't customize it and used as it was out of the box. Did a lot handling UI stuff as well (Buttons, Durationchanges,..) and the switching between sessions and breaks was implemented. Due different tasks at work and in our houshold I was distracted and forgott to commit changes regulary. Have to get a routine therefor in future.

--- timer/src/components/Timer.js
+++ timer/src/components/Timer.js
@@ -1,25 +1,188 @@
 import React from "react";
+import accurateInterval from "accurate-interval";
 
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, ButtonGroup, Container, Grid, IconButton, Typography } from "@mui/material";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
+import ReplayIcon from "@mui/icons-material/Replay";
 
-const Timer = () => {
-    return (
-        <React.Fragment>
-            <div id="timer-container">
-                <div id="timer">
-                    <Typography variant="h3" component="h1">Session-Timer</Typography>
-                    <Grid container spacing={3}>
-                        <Grid item xs={12} sm={6}>
-                            <Typography variant="h5" component="h2" id="session-label">Session</Typography>
+
+class Timer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            sessionLength: 25,
+            breakLength: 5,
+            timeTitle: "Session",
+            sessionCount: 0,
+            breakCount: 0,
+            time: 1500,
+            timer: "",
+            timerRunning: false 
+        }
+
+    }
+
+
+    /////////////
+    /* Methods */
+    /////////////
+
+    startTimer = () => {
+        this.setState({
+            timer: accurateInterval(() => {
+                this.decrementTime()
+                this.timerControl();
+                }, 1000)
+        });
+    }
+
+    decrementTime = () => {
+        this.setState({
+            time: this.state.time - 1
+        });
+    }
+
+    timerControl = () => {
+        if (this.state.time < 0 && this.state.timer && this.state.timeTitle === "Session") {
+            this.state.timer.clear();
+            this.setState({
+                timeTitle: "Break",
+                sessionCount: this.state.sessionCount + 1
+            });
+            this.calcDuration(this.state.breakLength);
+            this.startTimer();
+        }
+        if (this.state.time < 0 && this.state.timer && this.state.timeTitle === "Break") {
+            this.state.timer.clear();
+            this.setState({
+                timeTitle: "Session",
+                breakCount: this.state.breakCount + 1
+            });
+            this.calcDuration(this.state.sessionLength);
+            this.startTimer();
+        }
+    }
+
+
+    calcDuration = (duration) => {
+        this.setState({
+            time: duration*60
+        });
+    }
+
+    secsToClock = () => {
+        const mins = Math.floor(this.state.time / 60);
+        const secs = this.state.time - mins * 60;
+        return `${(mins < 10) ? '0' + mins : mins}:${(secs < 10)? '0' + secs : secs}`;
+    }
+
+    ////////////////////////////
+    /* Handle UI Interactions */
+    ////////////////////////////
+
+    handlePlayBtn = () => {
+        if (!this.state.timerRunning) {
+            this.startTimer(); 
+        } else {
+            if (this.state.timer) this.state.timer.clear(); 
+        }
+
+        this.setState({
+            timerRunning: !this.state.timerRunning
+        });
+
+    }
+
+    handleReset = () => {
+        if (this.state.timer) this.state.timer.clear();
+        this.setState({
+            sessionLength: 25,
+            breakLength: 5,
+            timeTitle: "Session",
+            sessionCount: 0,
+            breakCount: 0,
+            time: 1500,
+            timer: "",
+            timerRunning: false 
+        });
+    }
+
+    handleDurationButtons = (event) => {
+        switch (event.target.value) {
+            case 'breakUp':
+                if (this.state.breakLength < 60) this.setState({breakLength: this.state.breakLength + 1});
+                break;
+            case 'breakDown':
+                if (this.state.breakLength > 1) this.setState({breakLength: this.state.breakLength - 1});
+                break;
+            case 'sessionUp':
+                if (this.state.sessionLength < 60) this.setState({sessionLength: this.state.sessionLength + 1, time: this.state.time + 60});
+                break;
+            case 'sessionDown':
+                if (this.state.sessionLength > 1) this.setState({sessionLength: this.state.sessionLength - 1, time: this.state.time - 60});
+                break;
+            default:
+                return;
+        }
+    }
+
+
+    //////////////////////////////////
+    /* Let"s get the Party starting */
+    //////////////////////////////////
+
+    render() {
+        return (
+            <React.Fragment>
+                <div id="timer-container">
+                    <div id="timer">
+                        <Typography variant="h3" component="h1">Session-Timer</Typography>
+                    
+                        <Grid container spacing={3}>
+                            <Grid item xs={6}>
+                                <Typography variant="h5" component="h2" id="session-label">Session-Length</Typography>
+                            </Grid>    
+                            <Grid item xs={6}>
+                                <Typography variant="h5" component="h2" id="break-label">Break-Length</Typography>
+                            </Grid>    
+                            <Grid item xs={6}>
+                                <div className="time-adjust">
+                                    <Typography variant="h3" id="session-length">{this.state.sessionLength}</Typography>
+                                <ButtonGroup disableElevation variant="contained" orientation="vertical" size="small">
+                                    <Button value="sessionUp" onClick={this.handleDurationButtons} id="session-increment">Up</Button>
+                                    <Button value="sessionDown" onClick={this.handleDurationButtons} id="session-decrement">Down</Button>
+                                </ButtonGroup>
+                                </div>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <div className="time-adjust">
+                                    <Typography variant="h3" id="break-length">{this.state.breakLength}</Typography>
+                                    <ButtonGroup disableElevation variant="contained" orientation="vertical" size="small">
+                                        <Button value="breakUp"onClick={this.handleDurationButtons} id="break-increment">Up</Button>
+                                        <Button value="breakDown"onClick={this.handleDurationButtons} id="break-decrement">Down</Button>
+                                    </ButtonGroup>
+                                </div>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <Typography variant="h5" component="h2">Break</Typography>
-                        </Grid>
-                    </Grid>
+
+                        <div id="time-container">
+                            <Typography variant="h5" component="h2" pt={3} pb={0} id="timer-label">{this.state.timeTitle}</Typography>
+                            <Typography variant="h2" pt={1} id="time-left">{this.secsToClock(this.state.time)}</Typography>
+                        </div>
+                    
+                        <Container id="time-controls">
+                            <IconButton size="large"><PauseIcon fontSize="inherit"/></IconButton>
+                            <IconButton size="large" id="start_stop" onClick={this.handlePlayBtn}><PlayArrowIcon fontSize="inherit"/></IconButton>
+                            <IconButton size="large" id="reset" onClick={this.handleReset}><ReplayIcon fontSize="inherit"/></IconButton>
+                        </Container>
+
+                    </div>
                 </div>
-            </div>
-        </React.Fragment>
-    );
+            </React.Fragment>
+        
+        )
+    };
 }
 
 export default Timer;
